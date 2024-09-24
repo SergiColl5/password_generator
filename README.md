42daf0a3092f4bcd7f9eb76c92c37febd47b6ca2
SergiColl5 <sergicoll1995@gmail.com>
2024-09-24 16:45:10 +1000

    Code written to generate password meeting criteria.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a mini project to keep practising with python
---
+++ password_generator.py
@@ -0,0 +1,42 @@
+import random
+import string
+
+def generate_password(min_lengh,numbers=True,special_character=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_character:
+        characters+=special
+    
+    password=""
+    meets_chriteria = False
+    has_number=False
+    has_special=False
+
+    while not meets_chriteria or len(password)<min_lengh:
+        new_char=random.choice(characters)
+        password += new_char
+        if new_char in digits:
+            has_number=True
+        elif new_char in special:
+            has_special = True
+
+        meets_chriteria=True
+
+        if numbers:
+            meets_chriteria=has_number
+        if special_character:
+            meets_chriteria=meets_chriteria and has_special
+
+            
+
+    return password
+min_len = int(input("Minimum characters?:"))
+include_numbers = input("Do you want to include numbers? y/n: ").lower() == 'y'
+include_special = input("Do you want to include special characters? y/n: ").lower() == 'y'
+prova = generate_password(min_len,include_numbers,include_special)
+print(prova)
